a16397a0c4d2e30cf424ddf1b76c5c26e1ca9ed7
Hamza <Hamzanur2010@hotmail.com>
2021-11-25 02:46:31 +0000

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function (a1, b2) {
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] != b2[i]) {
+      return false
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (a1,b2) {
+  const isEqual = eqArrays ( a1, b2)
+  if(isEqual){
+    console.log('AssertionPassed'+a1+"==="+b2)
+  }else{
+    console.log('AssertionFailed'+a1+"!=="+b2)
+
+  }
+} 
