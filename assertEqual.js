d6fe98334f79ba1403bdc1d90fc9b56df30b3d65
Hamza <Hamzanur2010@hotmail.com>
2021-11-25 01:04:48 +0000

    removed console.log and added eqArrays.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(pizza,iceCream) {
   if (pizza === iceCream) {
-    console.log("😅Assertion Passed:" + pizza + "===" + iceCream);
+    console.log(`assertion Passed  ${pizza} === ${iceCream}`);
 
   } else {
-    console.log("😄Assertion failed:" + pizza + "!==" + iceCream);
+    console.log(`Assertion failed  ${pizza}  !==  ${iceCream}`);
 
   }
 };
---
+++ eqArrays.js
@@ -0,0 +1,10 @@
+const eqArrays = function (a1, b2) {
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] != b2[i]) {
+      return false
+    }
+  }
+  return true;
+
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
