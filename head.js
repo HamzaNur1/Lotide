0945030c4225cced3b214999b1d63568fcb0e069
Hamza <Hamzanur2010@hotmail.com>
2021-11-26 00:16:39 +0000

    head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(pizza,iceCream) {
+  if (pizza === iceCream) {
+    console.log(`assertion Passed  ${pizza} === ${iceCream}`);
+
+  } else {
+    console.log(`Assertion failed  ${pizza}  !==  ${iceCream}`);
+
+  }
+};
+const head = function(array){
+  return array[0]
+}
+//this is a empty function call
+assertEqual()
+//this is an empty function call
+head()
+//this calls assert equal function with head as first (piiza)as first parameter second as whatever you decide to put in there
+assertEqual(head([0,1,2,3]),0)
+assertEqual(head([0,1,2,3]),2)
