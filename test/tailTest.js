04aa52dd97d4d5a1010b08c147e78249dde01470
Hamza <Hamzanur2010@hotmail.com>
2021-11-30 22:45:44 +0000

    mocha and chai refactor

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
-const fruits = ['apples','oranges','grapes'];
-const results = tail(fruits)
-const expected = ['oranges','grapes']
-assertArraysEqual (results,expected)
+var assert = require('assert');
+describe('Array', function() {
+  describe('tail()',function() {
+    it('should return [orange ,grapes] for [apples,oranges and grapes]', function() {
+      let fruits = ['apples','oranges','grapes'];
+      let results = tail(fruits)
+      let expected = ['oranges','grapes'] 
+      assert.deepEqual (results, expected);
+    });
+    it('should return [orange ,grapes] for [apples,oranges and grapes]', function() {
+      let animals = ['lions','tiger','snake'];
+      let results = tail(animals)
+      let expected = ['tiger','snake'] 
+      assert.deepEqual (results, expected);
+    });
+  });
+});
